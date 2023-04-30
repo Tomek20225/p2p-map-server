22ac0968819015cca6b702a94118c0eb619c8d3c
Tomek20225 <tomek202255@gmail.com>
2023-04-30 02:45:41 +0200

    Linting

--- src/index.ts
+++ src/index.ts
@@ -10,20 +10,20 @@ dotenv.config()
 const port: number = parseInt(process.env.PORT)
 
 type Clients = {
-    [id: string]: Client
+	[id: string]: Client
 }
 
 type Scoreboard = {
-    [id: string]: number
+	[id: string]: number
 }
 
 type Client = {
-    t: number,
-    p: {
-        x: number,
-        y: number,
-        z: number
-    }
+	t: number
+	p: {
+		x: number
+		y: number
+		z: number
+	}
 }
 
 class App {
@@ -32,9 +32,9 @@ class App {
 
 	private io: Server
 	private clients: Clients = {}
-    private scoreboard: Scoreboard = {}
+	private scoreboard: Scoreboard = {}
 
-    private map: Maze
+	private map: Maze
 
 	constructor(port: number) {
 		this.port = port
@@ -47,15 +47,15 @@ class App {
 			})
 		)
 
-        const defaultRows = 11
-        const defaultCols = 11
-        const scaleFactor = 3
+		const defaultRows = 11
+		const defaultCols = 11
+		const scaleFactor = 3
 
-        // Maze setup
-        this.map = new Maze({
-            rows: defaultRows,
-            cols: defaultCols
-        })
+		// Maze setup
+		this.map = new Maze({
+			rows: defaultRows,
+			cols: defaultCols,
+		})
 
 		this.server = new http.Server(app)
 
@@ -70,21 +70,21 @@ class App {
 			console.log(this.clients)
 			console.log('a user connected : ' + socket.id)
 
-            this.clients[socket.id] = {} as Client
+			this.clients[socket.id] = {} as Client
 			this.scoreboard[socket.id] = 0
 
-            socket.emit('map', this.getParsedMapObject())
+			socket.emit('map', this.getParsedMapObject())
 			socket.emit('id', socket.id)
 
-            socket.on('disconnect', () => {
+			socket.on('disconnect', () => {
 				console.log('socket disconnected : ' + socket.id)
 				console.log('deleting ' + socket.id)
 
-                delete this.clients[socket.id]
+				delete this.clients[socket.id]
 				delete this.scoreboard[socket.id]
 
-                this.io.emit('removeClient', socket.id)
-                this.io.emit('scoreboard', this.scoreboard)
+				this.io.emit('removeClient', socket.id)
+				this.io.emit('scoreboard', this.scoreboard)
 			})
 
 			socket.on('update', (message: any) => {
@@ -94,54 +94,54 @@ class App {
 				}
 			})
 
-            this.io.emit('scoreboard', this.scoreboard)
+			this.io.emit('scoreboard', this.scoreboard)
 		})
 
-        // Updating positions of players
+		// Updating positions of players
 		setInterval(() => {
 			this.io.emit('clients', this.clients)
-        }, 50)
+		}, 50)
 
-        // Updating scores of players
+		// Updating scores of players
 		setInterval(() => {
 			this.io.emit('scoreboard', this.scoreboard)
-        }, 1000)
+		}, 1000)
 
-        // Checking for the winner
-        setInterval(() => {
-            const exitPos = this.map.getExit()
+		// Checking for the winner
+		setInterval(() => {
+			const exitPos = this.map.getExit()
 
-            for (const clientId of Object.keys(this.clients)) {
-                if (!this.clients[clientId].p) continue
+			for (const clientId of Object.keys(this.clients)) {
+				if (!this.clients[clientId].p) continue
 
-                const clientX = Math.round(this.clients[clientId].p.x)
-                const clientY = Math.round(this.clients[clientId].p.y)
+				const clientX = Math.round(this.clients[clientId].p.x)
+				const clientY = Math.round(this.clients[clientId].p.y)
 
-                if (!(clientX == exitPos.x && clientY == exitPos.y)) continue
+				if (!(clientX == exitPos.x && clientY == exitPos.y)) continue
 
-                this.io.emit('winner', clientId)
-                console.log(`winner ${clientId}`)
+				this.io.emit('winner', clientId)
+				console.log(`winner ${clientId}`)
 
-                this.scoreboard[clientId]++
+				this.scoreboard[clientId]++
 
-                const clientAmount = Object.keys(this.clients).length
-                let scaledRows = clientAmount * scaleFactor + Math.round((Math.random() * 4))
-                let scaledCols = clientAmount * scaleFactor + Math.round((Math.random() * 4))
-                scaledRows = scaledRows % 2 == 0 ? scaledRows + 1 : scaledRows
-                scaledCols = scaledCols % 2 == 0 ? scaledCols + 1 : scaledCols
+				const clientAmount = Object.keys(this.clients).length
+				let scaledRows = clientAmount * scaleFactor + Math.round(Math.random() * 4)
+				let scaledCols = clientAmount * scaleFactor + Math.round(Math.random() * 4)
+				scaledRows = scaledRows % 2 == 0 ? scaledRows + 1 : scaledRows
+				scaledCols = scaledCols % 2 == 0 ? scaledCols + 1 : scaledCols
 
-                this.map = new Maze({
-                    rows: scaledRows > defaultRows ? scaledRows : defaultRows,
-                    cols: scaledCols > defaultCols ? scaledCols : defaultCols
-                })
+				this.map = new Maze({
+					rows: scaledRows > defaultRows ? scaledRows : defaultRows,
+					cols: scaledCols > defaultCols ? scaledCols : defaultCols,
+				})
 
-                this.io.emit('map', this.getParsedMapObject())
-                this.io.emit('scoreboard', this.scoreboard)
+				this.io.emit('map', this.getParsedMapObject())
+				this.io.emit('scoreboard', this.scoreboard)
 
-                break
-            }
-        }, 100)
-    }
+				break
+			}
+		}, 100)
+	}
 
 	public Start() {
 		this.server.listen(this.port, () => {
@@ -149,16 +149,16 @@ class App {
 		})
 	}
 
-    private getParsedMapObject() {
-        return {
-            map: this.map.getMatrix(),
-            walkablePositions: this.map.getWalkablePositions(),
-            entrance: this.map.getEntrance(),
-            exit: this.map.getExit(),
-            width: this.map.getWidth(),
-            height: this.map.getHeight()
-        }
-    }
+	private getParsedMapObject() {
+		return {
+			map: this.map.getMatrix(),
+			walkablePositions: this.map.getWalkablePositions(),
+			entrance: this.map.getEntrance(),
+			exit: this.map.getExit(),
+			width: this.map.getWidth(),
+			height: this.map.getHeight(),
+		}
+	}
 }
 
 new App(port).Start()
--- src/maze.ts
+++ src/maze.ts
@@ -1,123 +1,126 @@
 export interface MazeOptions {
-  rows: number;
-  cols: number;
+	rows: number
+	cols: number
 }
 
 interface Vector2 {
-  x: number,
-  y: number
+	x: number
+	y: number
 }
 
 export default class Maze {
-  private rows: number;
-  private cols: number;
-  private entrance: Vector2;
-  private exit: Vector2;
-  private matrix: number[][];
-  private walkablePositions: Vector2[];
+	private rows: number
+	private cols: number
+	private entrance: Vector2
+	private exit: Vector2
+	private matrix: number[][]
+	private walkablePositions: Vector2[]
 
-  constructor({ rows, cols }: MazeOptions) {
-    this.rows = rows;
-    this.cols = cols;
-    this.entrance = {x: 1, y: 1};
-    this.matrix = Array.from({ length: this.rows }, () => Array.from({ length: this.cols }, () => 1));
+	constructor({ rows, cols }: MazeOptions) {
+		this.rows = rows
+		this.cols = cols
+		this.entrance = { x: 1, y: 1 }
+		this.matrix = Array.from({ length: this.rows }, () =>
+			Array.from({ length: this.cols }, () => 1)
+		)
 
-    this.generate();
+		this.generate()
 
-    this.walkablePositions = this.matrix.reduce((acc, currentValue, index) => {
-		let walkablePositionsInRow: Vector2[] = [];
-        for (let i = 0; i < currentValue.length; i++) {
-			if (currentValue[i] == 0) walkablePositionsInRow.push({x: i, y: -index});
-        }
-        return [...acc, ...walkablePositionsInRow];
-	}, [] as Vector2[]);
-    this.exit = this.getRandomExit();
-  }
+		this.walkablePositions = this.matrix.reduce((acc, currentValue, index) => {
+			let walkablePositionsInRow: Vector2[] = []
+			for (let i = 0; i < currentValue.length; i++) {
+				if (currentValue[i] == 0) walkablePositionsInRow.push({ x: i, y: -index })
+			}
+			return [...acc, ...walkablePositionsInRow]
+		}, [] as Vector2[])
+		this.exit = this.getRandomExit()
+	}
 
-  public getMatrix(): number[][] {
-    return this.matrix;
-  }
+	public getMatrix(): number[][] {
+		return this.matrix
+	}
 
-  private generate(): void {
-    const stack: [number, number][] = [[this.entrance.x, this.entrance.y]];
+	private generate(): void {
+		const stack: [number, number][] = [[this.entrance.x, this.entrance.y]]
 
-    while (stack.length > 0) {
-      const current = stack.pop()!;
-      const [row, col] = current;
-      this.matrix[row][col] = 0;
+		while (stack.length > 0) {
+			const current = stack.pop()!
+			const [row, col] = current
+			this.matrix[row][col] = 0
 
-      const neighbors = this.getUnvisitedNeighbors(row, col);
-      if (neighbors.length > 0) {
-        stack.push(current);
-        const [neighborRow, neighborCol] = neighbors[Math.floor(Math.random() * neighbors.length)];
-        this.removeWall(current, [neighborRow, neighborCol]);
-        stack.push([neighborRow, neighborCol]);
-      }
-    }
-  }
+			const neighbors = this.getUnvisitedNeighbors(row, col)
+			if (neighbors.length > 0) {
+				stack.push(current)
+				const [neighborRow, neighborCol] =
+					neighbors[Math.floor(Math.random() * neighbors.length)]
+				this.removeWall(current, [neighborRow, neighborCol])
+				stack.push([neighborRow, neighborCol])
+			}
+		}
+	}
 
-  private getUnvisitedNeighbors(row: number, col: number): [number, number][] {
-    const neighbors: [number, number][] = [];
+	private getUnvisitedNeighbors(row: number, col: number): [number, number][] {
+		const neighbors: [number, number][] = []
 
-    if (row > 1 && this.matrix[row - 2][col] === 1) {
-      neighbors.push([row - 2, col]);
-    }
-    if (col > 1 && this.matrix[row][col - 2] === 1) {
-      neighbors.push([row, col - 2]);
-    }
-    if (row < this.rows - 2 && this.matrix[row + 2][col] === 1) {
-      neighbors.push([row + 2, col]);
-    }
-    if (col < this.cols - 2 && this.matrix[row][col + 2] === 1) {
-      neighbors.push([row, col + 2]);
-    }
+		if (row > 1 && this.matrix[row - 2][col] === 1) {
+			neighbors.push([row - 2, col])
+		}
+		if (col > 1 && this.matrix[row][col - 2] === 1) {
+			neighbors.push([row, col - 2])
+		}
+		if (row < this.rows - 2 && this.matrix[row + 2][col] === 1) {
+			neighbors.push([row + 2, col])
+		}
+		if (col < this.cols - 2 && this.matrix[row][col + 2] === 1) {
+			neighbors.push([row, col + 2])
+		}
 
-    return neighbors;
-  }
+		return neighbors
+	}
 
-  private removeWall(cell1: [number, number], cell2: [number, number]): void {
-    const [row1, col1] = cell1;
-    const [row2, col2] = cell2;
+	private removeWall(cell1: [number, number], cell2: [number, number]): void {
+		const [row1, col1] = cell1
+		const [row2, col2] = cell2
 
-    const wallRow = (row1 + row2) / 2;
-    const wallCol = (col1 + col2) / 2;
+		const wallRow = (row1 + row2) / 2
+		const wallCol = (col1 + col2) / 2
 
-    this.matrix[wallRow][wallCol] = 0;
-  }
+		this.matrix[wallRow][wallCol] = 0
+	}
 
-  public getWalkablePositions(): Vector2[] {
-    return this.walkablePositions;
-  }
+	public getWalkablePositions(): Vector2[] {
+		return this.walkablePositions
+	}
 
-  public getRandomWalkablePosition(): Vector2 {
-    return this.walkablePositions[Math.floor(Math.random() * this.walkablePositions.length)];
-  }
+	public getRandomWalkablePosition(): Vector2 {
+		return this.walkablePositions[Math.floor(Math.random() * this.walkablePositions.length)]
+	}
 
-  private getRandomExit(): Vector2 {
-    let properPos = false
-    let randomPos: Vector2
+	private getRandomExit(): Vector2 {
+		let properPos = false
+		let randomPos: Vector2
 
-    while (!properPos) {
-        randomPos = this.getRandomWalkablePosition()
-        properPos = (randomPos.x != this.entrance.x && randomPos.y != this.entrance.y)
-    }
+		while (!properPos) {
+			randomPos = this.getRandomWalkablePosition()
+			properPos = randomPos.x != this.entrance.x && randomPos.y != this.entrance.y
+		}
 
-    return randomPos
-  }
+		return randomPos
+	}
 
-  public getExit(): Vector2 {
-    return this.exit;
-  }
+	public getExit(): Vector2 {
+		return this.exit
+	}
 
-  public getEntrance(): Vector2 {
-    return {x: this.entrance.x, y: -this.entrance.y};
-  }
+	public getEntrance(): Vector2 {
+		return { x: this.entrance.x, y: -this.entrance.y }
+	}
 
-  public getWidth(): number {
-    return this.cols
-  }
+	public getWidth(): number {
+		return this.cols
+	}
 
-  public getHeight(): number {
-    return this.rows
-  }
+	public getHeight(): number {
+		return this.rows
+	}
 }
